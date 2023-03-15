bfa7529c9677932a61305ed24f43e94042f83e09
nurima3001 <126258902+nurima3001@users.noreply.github.com>
2023-03-15 21:08:53 +0700

    Add files via upload

---
+++ DECISION/script.js
@@ -0,0 +1,51 @@
+const nilaiKeaktifan = 100,
+        nilaiUjian = 100,
+        nilaiAbsensi = 100;
+
+if (nilaiKeaktifan == 100
+    && nilaiUjian == 100
+    && nilaiAbsensi == 100
+) {
+    console.log ('Outstanding')
+} else {
+    console.log (' tidak outstanding')
+}
+
+if ( nilaiKeaktifan >= 95
+    && nilaiUjian >= 96
+    && nilaiAbsensi >= 96
+) {
+    console.log ( 'Excellent')
+} else {
+    console.log ('tidak excellent')
+}
+
+if ( nilaiKeaktifan >= 85
+    && nilaiUjian >= 83
+    && nilaiAbsensi >= 88
+) {
+    console.log ('Good')
+} else {
+    console.log ('tidak Good')
+}
+
+if (nilaiKeaktifan <= 70
+    && nilaiUjian <= 70
+    && nilaiAbsensi <= 70
+    ){
+        console.log (' Participant')
+} else {
+        console.log ('tidak Participant')
+ }
+ if (nilaiKeaktifan == 100 + nilaiUjian == 100 + nilaiAbsensi == 100)
+
+ return "Outstanding"
+
+ if (nilaiKeaktifan >= 95 + nilaiUjian >= 96 + nilaiAbsensi >= 96)
+
+ return "Excellent"
+
+ if (nilaiKeaktifan >= 83 + nilaiUjian >= 85 + nilaiAbsensi >= 88)
+
+ return "Good"
+ 
